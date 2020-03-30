062dbbefd74320c893fcf128c41cd067987b4330
github username <github email address>
2020-03-30 14:05:42 -0400

    commit

---
+++ db/migrate/001_create_students.rb
@@ -0,0 +1,15 @@
+class CreateStudents < ActiveRecord::Migration[5.2] 
+    def change 
+        create_table :students do |t| 
+            t.string :last_name 
+            t.string :first_name 
+            t.datetime :date_of_birth 
+            t.string :address 
+            t.string :city 
+            t.string :state 
+            t.string :country 
+            t.integer :credits 
+            t.double :gpa 
+        end 
+    end 
+end 
